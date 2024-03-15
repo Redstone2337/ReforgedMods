0b1c56ce8d514d4f49ab9f66de89b7826206aa99
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-03-15 22:01:07 +0800

    Update ModToolMaterial.java

--- src/main/java/com/redstone233/spawn/reforged/items/ModToolMaterial.java
+++ src/main/java/com/redstone233/spawn/reforged/items/ModToolMaterial.java
@@ -6,7 +6,7 @@ import net.minecraft.item.ToolMaterial;
 import net.minecraft.recipe.Ingredient;
 
 public enum ModToolMaterial implements ToolMaterial {
-    REFORGED_ITEM(5,600,4.5f,3.5f,25,
+    REFORGED_ITEM(5,600,6.25f,7.25f,25,
     () -> Ingredient.ofItems(ModItems.REFORGED_ITEM))
     ;
 
