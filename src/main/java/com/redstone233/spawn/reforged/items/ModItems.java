0f0c3ad34b19af4dcb5ad1baf4ea9cac69f8f27b
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-03-15 21:57:35 +0800

    Update ModItems.java

--- src/main/java/com/redstone233/spawn/reforged/items/ModItems.java
+++ src/main/java/com/redstone233/spawn/reforged/items/ModItems.java
@@ -30,23 +30,23 @@ public class ModItems {
         new Prospectetor(new FabricItemSettings().maxDamage(64)));
 
     public static final Item REFORGED_PICKAXE = registerItems("reforged_pickaxe", 
-        new PickaxeItem(ModToolMaterial.REFORGED_ITEM, 5, 2.3f, 
+        new PickaxeItem(ModToolMaterial.REFORGED_ITEM, 5, -1.3f, 
             new FabricItemSettings()));
     
     public static final Item REFORGED_AXE = registerItems("reforged_axe", 
-        new AxeItem(ModToolMaterial.REFORGED_ITEM, 11, 5.2f, 
+        new AxeItem(ModToolMaterial.REFORGED_ITEM, 15, 5.1f, 
             new FabricItemSettings()));
 
     public static final Item REFORGED_SHOVRL = registerItems("reforged_shovel", 
-        new ShovelItem(ModToolMaterial.REFORGED_ITEM, 3, 3.2f, 
+        new ShovelItem(ModToolMaterial.REFORGED_ITEM, 3, -1.2f, 
             new FabricItemSettings()));    
 
     public static final Item REFORGED_SWORD = registerItems("reforged_sword", 
-        new SwordItem(ModToolMaterial.REFORGED_ITEM, 10, 1.5f, 
+        new SwordItem(ModToolMaterial.REFORGED_ITEM, 10, -1.5f, 
             new FabricItemSettings()));
 
     public static final Item REFORGED_HOE = registerItems("reforged_hoe", 
-        new HoeItem(ModToolMaterial.REFORGED_ITEM, 6, 3.1f, 
+        new HoeItem(ModToolMaterial.REFORGED_ITEM, 6, -1.8f, 
             new FabricItemSettings()));
 
     public static final Item REFORGED_HELMET = registerItems("reforged_helmets",
