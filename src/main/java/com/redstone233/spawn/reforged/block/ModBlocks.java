5f336e4debf59daa54067670e72b9bad6a0049f0
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-03-17 06:58:13 +0800

    Update ModBlocks.java

--- src/main/java/com/redstone233/spawn/reforged/block/ModBlocks.java
+++ src/main/java/com/redstone233/spawn/reforged/block/ModBlocks.java
@@ -27,11 +27,11 @@ import net.minecraft.util.Identifier;
 public class ModBlocks {
 
     public static final Block REFORGED_BLOCK = 
-        registerBlocks("reforged_block", 
+        registerBlocks("reforged_block1", 
         new Block(FabricBlockSettings.copyOf(Blocks.STONE)));
 
     public static final Block REFORGED_BLOCK1 = 
-        registerBlocks("reforged_block1", 
+        registerBlocks("reforged_block", 
         new Block(FabricBlockSettings.copyOf(Blocks.STONE)));
 
     public static final Block REFORGED_STAIR = 
