9fe407d1412ae4841122f106987cf2e249a0873c
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-03-20 13:56:57 +0800

    Create ModWorldGeneration.java

---
+++ src/main/java/com/redstone233/spawn/reforged/world/gen/ModWorldGeneration.java
@@ -0,0 +1,7 @@
+package com.redstone233.spawn.reforged.world.gen;
+
+public class ModWorldGeneration {
+    public static void generateModWorldGen(){
+        ModOreGeneration.generateOres();
+    }
+}
