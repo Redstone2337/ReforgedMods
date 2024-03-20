c4c1c1952610b6bb5270d6b7ba57b866817e1456
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-03-20 15:20:50 +0800

    Add files via upload

--- src/main/java/com/redstone233/spawn/reforged/block/ModBlocks.java
+++ src/main/java/com/redstone233/spawn/reforged/block/ModBlocks.java
@@ -24,6 +24,7 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
 
