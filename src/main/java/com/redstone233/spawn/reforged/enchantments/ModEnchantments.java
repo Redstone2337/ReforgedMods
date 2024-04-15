27901319804f42fe49087bcfd75b27049880269f
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-04-15 12:02:04 +0800

    Add files via upload

---
+++ src/main/java/com/redstone233/spawn/reforged/enchantments/ModEnchantments.java
@@ -0,0 +1,43 @@
+package com.redstone233.spawn.reforged.enchantments;
+
+import com.redstone233.spawn.reforged.ReforgeSpawnMod;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentTarget;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModEnchantments {
+
+
+    public static final Enchantment VAMPRICM_ENCHANTMENT = 
+        registerModEnchantments("vampiricm",
+            new VampiricmEnchantment(Enchantment.Rarity.COMMON, EnchantmentTarget.WEAPON,
+                new EquipmentSlot[] {
+                    EquipmentSlot.MAINHAND,
+                    EquipmentSlot.OFFHAND
+                }
+            )
+        );
+
+    public static final Enchantment FROZEN = 
+        registerModEnchantments("frozen",
+            new FrozenEnchantment(Enchantment.Rarity.COMMON,EnchantmentTarget.WEAPON,
+                new EquipmentSlot[] {
+                    EquipmentSlot.MAINHAND,
+                    EquipmentSlot.OFFHAND
+                }
+            )
+        );
+
+    private static Enchantment registerModEnchantments(String name,Enchantment enchantment) {
+        return Registry.register(Registries.ENCHANTMENT, 
+            new Identifier(ReforgeSpawnMod.MOD_ID, name),enchantment);
+    }
+
+    public static void registerModEnchantments() {
+        
+    }
+}
