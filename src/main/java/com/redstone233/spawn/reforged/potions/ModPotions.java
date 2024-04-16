b069421a184f033c183342ceff6cf8da15a9cfb7
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-04-16 20:59:11 +0800

    Update ModPotions.java

--- src/main/java/com/redstone233/spawn/reforged/potions/ModPotions.java
+++ src/main/java/com/redstone233/spawn/reforged/potions/ModPotions.java
@@ -13,10 +13,10 @@ public class ModPotions {
 
 
     public static final Potion FIRE_POTION = registerModPotions("fire_potion", 
-        new FirePotion(new StatusEffectInstance[]{new StatusEffectInstance(ModEffects.FIRE_STATUS_EFFECT)}));
+        new FirePotion(new StatusEffectInstance(ModEffects.FIRE_STATUS_EFFECT,600)));
 
     public static final Potion GREAT_FIRE_POTION = registerModPotions("great_fire_potion", 
-        new Potion(new StatusEffectInstance(ModEffects.GREAT_FIRE_STATUS,600)));
+        new Potion(new StatusEffectInstance(ModEffects.GREAT_FIRE_STATUS,1800)));
 
 
     private static Potion registerModPotions(String name, Potion potion) {
