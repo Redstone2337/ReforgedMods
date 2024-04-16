1fe66bb02dc9a397aedb3b9f0345f06d98208eab
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-04-16 21:37:34 +0800

    Update GreatFireEffect.java

--- src/main/java/com/redstone233/spawn/reforged/effects/GreatFireEffect.java
+++ src/main/java/com/redstone233/spawn/reforged/effects/GreatFireEffect.java
@@ -24,6 +24,22 @@ public class GreatFireEffect extends StatusEffect {
     }
 
     @Override
+    public void applyEffectTick(LivingEntity entity, int amplifier) {
+        // 每刻（tick）对实体应用效果
+        // 在这里，我们可以添加火焰附加效果
+        if (entity.isOnFire()) {
+            // 如果实体已经在燃烧，我们就不需要再次点燃
+            return;
+        }
+        // 有概率使实体着火
+        // if (world.random.nextFloat() < (0.1 * (amplifier + 1))) {
+           // entity.setFire(1);
+        // }
+        // 提供生命恢复
+        entity.heal(1.0f / (amplifier + 1));
+    }
+
+    @Override
     public void onApplied(LivingEntity entity, int amplifier) {
         if (entity.isOnFire()) {
             entity.clearStatusEffects();
