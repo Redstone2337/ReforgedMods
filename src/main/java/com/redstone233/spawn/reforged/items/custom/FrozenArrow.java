37ecd5243cb8d3b0c1b1ff90fc63d6a44b73977e
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-04-17 17:00:51 +0800

    Add files via upload

---
+++ src/main/java/com/redstone233/spawn/reforged/items/custom/FrozenArrow.java
@@ -0,0 +1,21 @@
+package com.redstone233.spawn.reforged.items.custom;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+public class FrozenArrow extends Item {
+
+    public FrozenArrow(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        target.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS,200,50));
+        return true;
+    }
+
+}
