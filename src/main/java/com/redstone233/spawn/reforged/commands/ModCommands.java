c8ace99d4e40985c77848f8c389c87dcfc4986df
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-04-18 23:18:35 +0800

    Update ModCommands.java

--- src/main/java/com/redstone233/spawn/reforged/commands/ModCommands.java
+++ src/main/java/com/redstone233/spawn/reforged/commands/ModCommands.java
@@ -14,7 +14,7 @@ public class ModCommands {
 
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         LiteralArgumentBuilder<ServerCommandSource> builder =
-            CommandManager.literal("trsm").executes(ModCommands::run)
+            CommandManager.literal("reforgedspawn").executes(ModCommands::run)
             .then(LongCommand.register())
             .then(NbtCommand.register())
             .then(TrsmCommand.register())
