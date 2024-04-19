37e7d1967e71a80b5bea77bdb5663ec195837fa6
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-04-19 12:54:00 +0800

    Update ModToolMaterial.java

--- src/main/java/com/redstone233/spawn/reforged/items/ModToolMaterial.java
+++ src/main/java/com/redstone233/spawn/reforged/items/ModToolMaterial.java
@@ -6,9 +6,12 @@ import net.minecraft.item.ToolMaterial;
 import net.minecraft.recipe.Ingredient;
 
 public enum ModToolMaterial implements ToolMaterial {
+    REFORGED_AMETHYST(6,610,5.0f,4.0f,30,
+    () -> Ingredient.ofItems(ModItems.REFORGED_AMETHYST)),
+    ICE_DRAGON_SCALES(9,660,5.5f,4.5f,30,
+    () -> Ingredient.ofItems(ModItems.ICE_DRAGON_SCALES)),
     REFORGED_ITEM(5,600,4.5f,3.5f,25,
-    () -> Ingredient.ofItems(ModItems.REFORGED_ITEM))
-    ;
+    () -> Ingredient.ofItems(ModItems.REFORGED_ITEM));
 
     private final int miningLevel;
     private final int itemDurability;
