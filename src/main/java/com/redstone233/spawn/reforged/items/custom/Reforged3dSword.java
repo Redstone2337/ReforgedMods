cdeab725639ad38af0082de91d531ab206b1b499
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-04-20 21:57:07 +0800

    Add files via upload

---
+++ src/main/java/com/redstone233/spawn/reforged/items/custom/Reforged3dSword.java
@@ -0,0 +1,34 @@
+package com.redstone233.spawn.reforged.items.custom;
+
+import java.util.List;
+
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.text.Text;
+import net.minecraft.world.World;
+
+public class Reforged3dSword extends SwordItem {
+
+    public Reforged3dSword(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
+        super(toolMaterial, attackDamage, attackSpeed, settings);
+    }
+
+    @Override
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        target.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS,200,255));
+        return true;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable("item.trsm.3d.tooltip1"));
+        tooltip.add(Text.translatable("item.trsm.3d.tooltip2"));
+        super.appendTooltip(stack, world, tooltip, context);
+    }
+
+}
